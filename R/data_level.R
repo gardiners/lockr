4477f452bb19f2fa3221b24efd73fb2bc09ebac0
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2021-08-06 21:23:34 +1000

    Added object documentation.

--- R/data_level.R
+++ R/data_level.R
@@ -1,3 +1,15 @@
+#' Perform data-level locking operations
+#'
+#' @param record A vector of REDCap record names to affect. Required.
+#' @param event A character vector specifying the event/s to affect. Ignored if REDCap project is not longitudinal
+#' @param instrument A character vector specifying the instrument/s to affect. If NULL, all instruments for the given record/event specification are affected
+#' @param instance A vector specifying which instance of a repeating instrument to affect. If not specified, affects every instance.
+#' @param arm A vector specifying which arm to affect.
+#' @param action A character string for the action to perform on records. One of "lock", "unlock" or "status"
+#' @param redcap_uri A character string of the REDCap server's API endpoint URL
+#' @param token A character string of the REDCap API token
+#'
+#' @export
 data_action <- function(record, event = NULL, instrument = NULL, instance = NULL,
                         arm = NULL, action, redcap_uri, token) {
 
@@ -8,20 +20,27 @@ data_action <- function(record, event = NULL, instrument = NULL, instance = NULL
              function(payload) request(target, payload))
 }
 
+#' @describeIn data_action Get the lock status of specified instruments
+#' @export
+data_status <- function(record, event = NULL, instrument = NULL, instance = NULL,
+                        arm = NULL, redcap_uri, token) {
+  do.call(data_action, args = c(as.list(environment()),
+                                action = "status"))
+}
+
+#' @describeIn data_action Lock specified instruments
+#' @export
 lock_data <- function(record, event = NULL, instrument = NULL, instance = NULL,
                       arm = NULL, redcap_uri, token) {
   do.call(data_action, args = c(as.list(environment()),
                                 action = "lock"))
 }
 
+#' @describeIn data_action Unlock specified instruments
+#' @export
 unlock_data <- function(record, event = NULL, instrument = NULL, instance = NULL,
                         arm = NULL, redcap_uri, token) {
   do.call(data_action, args = c(as.list(environment()),
                                 action = "unlock"))
 }
 
-data_status <- function(record, event = NULL, instrument = NULL, instance = NULL,
-                        arm = NULL, redcap_uri, token) {
-  do.call(data_action, args = c(as.list(environment()),
-                                action = "status"))
-}
--- R/record_level.R
+++ R/record_level.R
@@ -1,17 +1,30 @@
+#' Perform record-level locking operations
+#'
+#' @param action A character string for the action to perform on records. One of "lock", "unlock" or "status"
+#'
+#' @return A dataframe with one row per record affected by the operation.
+#' @export
+#' @inheritParams data_action
 record_action <- function(record, action = c("status", "lock", "unlock"), redcap_uri, token) {
   target <- make_url(redcap_uri, action)
   payload <- record_payload(token, record)
   request(target, payload)
 }
 
+#' @describeIn record_action Return the lock status of record/s
+#' @export
+record_status <- function(record, redcap_uri, token) {
+  record_action(record, action = "status", redcap_uri = redcap_uri, token = token)
+}
+
+#' @describeIn record_action Lock named record/s
+#' @export
 lock_record <-function(record, redcap_uri, token) {
   record_action(record, action = "lock", redcap_uri = redcap_uri, token = token)
 }
 
+#' @describeIn record_action Unlock named record/s
+#' @export
 unlock_record <- function(record, redcap_uri, token) {
   record_action(record, action = "unlock", redcap_uri = redcap_uri, token = token)
-}
-
-record_status <- function(record, redcap_uri, token) {
-  record_action(record, action = "status", redcap_uri = redcap_uri, token = token)
 }
